ba871f96207b5b1663a355b6725b71182a13fef3
Nikos Tziralis <ntziralis@gmail.com>
2021-10-01 11:47:34 +0300

    nft/reject endpoint

--- api/controllers/NftController.js
+++ api/controllers/NftController.js
@@ -142,7 +142,7 @@ module.exports = {
             data: {}
         };
 
-        const nftForm = await NFTForm.findOne({id: req.body.id})
+        const nftForm = await NFTForm.findOne({ id: req.body.id })
         if (nftForm.current_status !== 'approved') {
             res_obj.success = false;
             res_obj.message = "NFT has not been approved";
@@ -156,9 +156,6 @@ module.exports = {
         if (Array.isArray(issued)) {
             let isSuccess = issued.every(txIsSuccess);
             let isAccepted = issued.every(txIsAccepted);
-
-            console.log('isSuccess', isSuccess)
-            console.log('isAccepted', isAccepted)
 
             if (isSuccess && isAccepted) {
                 const updateNftStatusResponse = await NFTFormService.updateStatus('issued', req.body.id)
@@ -266,7 +263,7 @@ module.exports = {
 
         let statusAssociationOptions = { where: { id: updateNftStatusResponse.nft_form_status.id } }
         let xummAssociationOptions = { where: { id: xumm_api_payload.id } }
-   
+
         const nftPopulated = await sails.models.nftform.findOne({ "id": req.body.id })
             .populate('status', statusAssociationOptions)
             .populate('xumm', xummAssociationOptions)
@@ -292,6 +289,27 @@ module.exports = {
 
     },
 
+    reject: async function (req, res) {
+        let res_obj = {
+            success: false,
+            message: "",
+            data: {}
+        };
+
+        const updateNftStatusResponse = await NFTFormService.updateStatus('rejected', req.body.id)
+        if (!updateNftStatusResponse.success) {
+            res_obj.success = false;
+            res_obj.message = "NFT does not exist";
+
+            return res.badRequest(res_obj);
+        }
+        res_obj.success = true
+        res_obj.message = "NFT rejected successfully"
+        res_obj.data = { nft: { id: req.body.id } }
+
+        return _requestRes(res_obj, res)
+    },
+
     find: async function (req, res) {
 
         let res_obj = {
@@ -308,15 +326,15 @@ module.exports = {
         var NFTOptions = {
             where: {}
         }
-        
+
         let sortBy = req.query.sortBy || 'createdAt'
         let order = req.query.order || 'asc'
         let pageSize = req.query.pageSize || 10
         let page = req.query.page || 1
-        
+
         if (req.query.id) NFTOptions.where.id = req.query.id
         if (req.query.status) NFTOptions.where.current_status = req.query.status
-        
+
         allNFT = await sails.models.nftform.find()
             .where(NFTOptions.where)
             .populate('status', associationOptions)
@@ -346,21 +364,21 @@ module.exports = {
             message: "",
             data: {}
         };
-        
+
         console.log('here')
         nft = await sails.models.nftform.findOne({ id: req.query.id })
         const allowedToBeChanged = [
             'details.token_name',
             'details.url'
         ]
-        
+
         console.log(req.body)
         console.log(Object.keys(req.body).every(allowedToBeChanged))
         // if (body.every(allowedToBeChanged))
-        
+
         res.ok()
         return
-        
+
         const nftUpdateValues = {
             $set: {
                 "balanceAvailable": balanceAvailable
